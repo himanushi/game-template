0429d25b4f693f87b22b72a23529697b6fc1093b
himanushi <himanushi@users.noreply.github.com>
2023-03-24 20:20:47 +0900

    fix

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { initialize } from '@ionic/core';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		initialize({
+			animated: true,
+			mode: 'ios'
+		});
+	});
+</script>
+
+<ion-app>
+	<ion-header>
+		<ion-toolbar />
+	</ion-header>
+	<ion-content scroll-x={false}>
+		<slot />
+	</ion-content>
+	<ion-footer>
+		<ion-toolbar />
+	</ion-footer>
+</ion-app>
